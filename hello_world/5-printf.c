29a5b7c130bec95d5a29065e598655547db09427
root <you@example.com>
2024-06-17 18:36:22 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -6,8 +7,10 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
