d4ce81ac26e66dedb00fe64fef4659dade1d3c30
root <you@example.com>
2024-06-17 14:28:27 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /**
 * main - Entry point
 *
-* Description 'The program's print text'
+* Description: 'the program's print text'
 *
-* Return Always 0 (Success)
+* Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
