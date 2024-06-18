20cf653f1aa453631cc7a361346ce3cada0231e2
root <you@example.com>
2024-06-18 09:30:34 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
@@ -7,7 +6,6 @@
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 printf("Size of char: %lu byte(s)\n", sizeof(char));
@@ -15,7 +13,5 @@ printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long int: %lu byte(s)\n", sizeof(long));
 printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
-
 return (0);
 }
-
