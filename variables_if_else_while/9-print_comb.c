c6b5871d9302fb830ea258d47ef2c414c69202de
root <you@example.com>
2024-06-19 12:49:29 +0000

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: 'Print combinaison'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int c,
+
+	for (c = 0; c <= 9; ++c)
+	{
+	putchar(c + '0');
+	}
+	for (c = 'a'; c <= 'f'; ++c)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
