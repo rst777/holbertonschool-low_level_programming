596636b828f3e5a90967fa261293c0074f531df3
root <you@example.com>
2024-06-19 09:29:13 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print number in base 10'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; ++c)
+	{
+	printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
