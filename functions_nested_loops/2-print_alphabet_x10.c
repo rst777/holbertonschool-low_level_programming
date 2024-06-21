ccaf1f8adc4ce33e6c8aec21b5f1f7af6afc4cb3
Your Name <you@example.com>
2024-06-21 08:04:02 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
 /**
-* print_alphabet - prints the alphabet 10 X
+* print_alphabet_x10 - prints the alphabet 10 times
 *
-* Description: Prints the alphabet from 'a' to 'z' followed by a newline.
+* Description: Prints the alphabet from 'a' to 'z' followed by a newline,
+* repeated 10 times.
 */
-
-void print_alphabet_x10(void);
-
-
 void print_alphabet_x10(void)
 {
-	int n; 
+	int n;
+
 	char letter;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-	{
+		{
 			_putchar(letter);
-	}
+		}
 		_putchar('\n');
 
 	}
