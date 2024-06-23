322c2b11ccfa60ffdf22bcad2efa941503bddc58
Your Name <you@example.com>
2024-06-23 17:20:25 +0000

    .

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+void times_table(void);
+{
+	int rang;
+	int colon;
+	int produit;
+
+	for (rang = 0; rang <= 9; rang++)
+	{	
+		_putchar('0');
+
+		for (colon = 1; colon <= 9; colon++)
+		{
+
+			produit = rang * colon;
+
+
+			
