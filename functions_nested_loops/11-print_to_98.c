1dc521ef6f2b3c3f9013792ec177478b85882d7d
Your Name <you@example.com>
2024-06-23 22:34:04 +0000

    .

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+
+void print_to_98(int n);
+
+{
+	if (n <= 98)
+	{
+		for(; n <= 98; n++)
+		{	
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf'("%d\n" , n);
