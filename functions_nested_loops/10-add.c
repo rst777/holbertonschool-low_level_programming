d2528eb389335c9f2f5783ffd11d8adb9f030b73
Your Name <you@example.com>
2024-06-23 22:00:29 +0000

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+* add - Adds two integers and prints the result using _putchar
+* @a: First integer
+* @b: Second integer
+*
+* Return: The result of the addition
+*/
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
