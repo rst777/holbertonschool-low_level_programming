e8272df11b25a8ffe602ee08b92a6f6b2ee6135d
Your Name <you@example.com>
2024-06-24 12:04:15 +0000

    .

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+* more_numbers - imprime 10 fois les nombres de 0Ã| 14.
+*/
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+
+			{
+
+				_putchar('1');
+
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
