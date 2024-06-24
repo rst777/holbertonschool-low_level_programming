a2bac4fa09e0167298861ca7b0a211692f6d5a63
Your Name <you@example.com>
2024-06-24 09:21:06 +0000

    .

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
+
+#endif /* MAIN_H */
