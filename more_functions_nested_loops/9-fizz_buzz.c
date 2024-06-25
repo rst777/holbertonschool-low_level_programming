bad43bbcf738ad65c5b41defd9c9941721e517b5
Your Name <you@example.com>
2024-06-25 12:41:49 +0000

    .

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,29 @@ int main(void)
 	int  i;
 
 		for (i = 1; i <= 100; i++)
-
+	{
 			if (i % 3 == 0 && i % 5 == 0)
 			{
 			printf("Fizz Buzz");
 	}
 	else if (i % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	else if (i % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
-		printf("%d ", i);
+		printf("%d", i);
 	}
-		if (i == 100)
+		if (i < 100)
 	{
 		printf(" ");
 	}
+}
 		printf("\n");
-
 		return (0);
 
 }
