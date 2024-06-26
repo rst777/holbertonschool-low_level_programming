19ef53548be293691830fcb25c02bd4bf573838d
Your Name <you@example.com>
2024-06-26 20:36:08 +0000

    .

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
 #include"main.h"
 #include <stdio.h>
+
 /**
- * _puts - Affiche une chaîne de caractères suivie d'un saut de ligne
- * @str: La chaîne de caractères à afficher
- *
- * Description: Cette fonction utilise la fonction `puts` pour afficher
- * la chaîne de caractères passée en argnt, suivie d'un saut de ligne.
+ * _puts - Imprime une chaîne de caractères sur stdo
+ * @str: La chaîne de caractères à imprimer.
  */
 
 void _puts(char *str)
 
 {
-	puts(str);
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
