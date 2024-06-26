be5e949d1660566e06c3dac156b01b1c3d3c830f
Your Name <you@example.com>
2024-06-26 20:20:28 +0000

    .

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * _puts - Affiche une chaîne de caractères suivie d'un saut de ligne
+ * @str: La chaîne de caractères à afficher
+ *
+ * Description: Cette fonction utilise la fonction `puts` pour afficher
+ * la chaîne de caractères passée en argnt, suivie d'un saut de ligne.
+ */
+
+void _puts(char *str)
+
+{
+	puts(str);
+}
