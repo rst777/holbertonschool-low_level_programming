d821f3a9ea50704740ee184bcde8ccabc7d05343
Your Name <you@example.com>
2024-06-26 19:37:35 +0000

    .

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - Calcule la longueur d'une chaîne de caractères.
+ * @s: La chaîne de caractères dont la longueur est calculée.
+ *
+ * Return: La longueur de la chaîne (nombre de caractères).
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+		return (i);
+
+}
