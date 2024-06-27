6bafaa584f8b7db67f093682886e88a90f0a9480
Your Name <you@example.com>
2024-06-27 15:21:57 +0000

    .

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * // Trouver la longueur de la chaîne str en parcourant les caractères
+ * Boucle pour imprimer chaque deuxième caractère de la chaîne
+ *  Ajouter un saut de ligne à la fin
+ *
+ */
+
+void puts2(char *str)
+
+{
+	int i;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
