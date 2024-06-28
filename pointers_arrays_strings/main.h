0dc24b7fbc7d5fd9c90ccba98dd2ada7c39f024b
Your Name <you@example.com>
2024-06-28 09:12:54 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,4 +10,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 #endif /* MAIN_H */
