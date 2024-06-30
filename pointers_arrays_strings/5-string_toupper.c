952350d8016feecc2cc758da4a41e6ee2018f946
Your Name <you@example.com>
2024-06-30 21:54:07 +0000

    5-string_toupper.c main.h

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_toupper - Convertit toutes les lettres minuscules d'une chaîne en majuscules.
+ * string_toupper - Convertit toutes les lettres min d'une chaîne en ma
  * @str: Chaîne à convertir.
  * Return: Pointeur vers la chaîne modifiée.
  */
@@ -8,14 +8,15 @@ char *string_toupper(char *str)
 
 {
 
-	int i =0;
+	int i = 0;
 
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
+
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = str[i] - ('a' - 'A'); /** Convertit en majuscule en ajustant la valeur ASCII */
+			str[i] = str[i] - ('a' - 'A');
 		}
 		i++;
 	}
