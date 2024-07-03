69e79529c279b9586d3457fabf4280592e7e645d
Your Name <you@example.com>
2024-07-03 10:10:06 +0200

    0-memeset.c for betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * _memset - Remplit les premiers n octets de la zone m√moire point√©e p
+ * _memset - Remplit les premiers n octets de la zone memoire pointee p
  *           avec l'octet constant b.
  *
- * @s: Pointeur vers la zone m√©moire √† remplir.
- * @b: L'octet de valeur √† utiliser pour remplir.
- * @n: Le nombre d'octets √† remplir avec cette valeur.
+ * @s: Pointeur vers la zone memoire a† remplir.
+ * @b: L'octet de valeur a† utiliser pour remplir.
+ * @n: Le nombre d'octets a† remplir avec cette valeur.
  *
- * Return: Un pointeur vers la zone m√©moire s.
+ * Return: Un pointeur vers la zone memoire s.
  */
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	char *p = s;
 
