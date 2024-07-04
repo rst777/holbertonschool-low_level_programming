593bcab1a09f6c2fd69a9fdc7e1321164d60d2f1
Your Name <you@example.com>
2024-07-04 23:56:44 +0200

    betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,25 +6,26 @@
  * @argc: Nombre d'arguments.
  * @argv: Tableau de chaînes contenant les arguments.
  *
- * Return: 0 si le programme réussit, 1 si le nombre d'arguments est incorrect.
+ * Return: 0 si le programme réussit,
+ * 1 si le nombre d'arguments est incorrect.
  */
 int main(int argc, char *argv[])
 
 {
-	
+
 	int num1, num2, result;
 
-	
+
 	if (argc != 3)
 	{
 	printf("Error\n");
-	return(1);
+	return (1);
 	}
-	num1 = atoi (argv[1]);
-	num2 = atoi (argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
-	
-	return(0);
+
+	return (0);
 }
