dc2494edcdb7cb17d0a2ce95c92d40a2af434b6a
Your Name <you@example.com>
2024-07-09 14:29:36 +0200

    4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - Libère la mémoire allouée pour un tableau 2D
+ * @grid: Pointeur vers le tableau 2D à libérer
+ * @height: Nombre de lignes du tableau 2D
+ *
+ * Description: Libère d'abord chaque ligne, puis le tableau de pointeurs.
+ */
+void free_grid(int **grid, int height)
+
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
