4b6d05c1e4fd483ea627a69fbc22d5deae6de269
Your Name <you@example.com>
2024-07-10 21:06:37 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Structure représentant un chien
- * @name: Pointeur vers une chaîne de caractères pour le nom du chien
- * @age: Âge du chien en flottant
- * @owner: Pointeur vers une chaîne de caractères
- * pour le nom du propriétaire
- */
-
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+
+char *name;
+float age;
+char *owner;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 };
 
 #endif /* DOG_H */
