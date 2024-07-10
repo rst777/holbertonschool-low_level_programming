d415b0e4a3be31d5fd7d81b2e4fe6cf37955c102
Your Name <you@example.com>
2024-07-10 23:24:48 +0200

    dog.h 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "dog.h"
+/**
+ * init_dog - Initialise une variable de type struct dog
+ * @d: Pointeur vers la structure à initialiser
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Nom du propriétaire du chien
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
