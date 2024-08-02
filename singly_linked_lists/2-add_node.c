710c6bf4523dcd1cdfc8a655785de7fd93ee3fc3
Your Name <you@example.com>
2024-08-02 15:11:03 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,51 +1,44 @@
 #include "lists.h"
 #include <string.h>
+#include <stdlib.h>
 /**
- * add_node - Adds a new node at the beginning of a list.
- * @head: Address of the pointer to the first node of the list.
- * @str: String to be added to the new node.
+ * add_node - Ajoute un nouveau nœud au début de la liste_t.
+ * @head: Double pointeur vers la tête de la liste.
+ * @str: Chaîne de caractères à dupliquer et ajouter au nouveau nœud.
  *
- * Return: The address of the new node if successful, or NULL if it fails.
- *
- * Description: This function allocates memory for a new node, duplicates
- * the string passed as a parameter, updates the pointers to insert the
- * new node at the beginning of the list, and returns the address of the
- * new node. If memory allocation or string duplication fails, it frees
- * the allocated memory and returns NULL.
+ * Return: Adresse du nouvel élément, ou NULL si l'ajout échoue.
  */
 
 list_t *add_node(list_t **head, const char *str)
 
 {
 	list_t *new_node;
-/* Allouer la memoire */
+	char *new_str;
+	int len;
 
+	/* Dupliquer la chaîne de caractères */
+	new_str = strdup(str);
+	if (new_str == NULL)
+		return (NULL);
+
+	/* Calculer la longueur de la chaîne */
+	len = strlen(str);
+
+	/* Allouer de la mémoire pour le nouveau node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return (NULL);
-
-/* Dupliquer la chaine*/
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
 	{
-		free(new_node);
+		free(new_str); /* Libérer la mémoire allouée pour la chaîne */
 		return (NULL);
 	}
-/* Assigner la longueur de la chaine */
 
-	new_node->len = strlen(str);
-
-/* Mettre à jour le nouveau node */
-
+	/* Initialiser le nouveau node */
+	new_node->str = new_str;
+	new_node->len = len;
 	new_node->next = *head;
-
-/* Mettre à jour le head de la liste*/
-
 	*head = new_node;
-
-/* Retourner l'adresse du new_node */
 
 	return (new_node);
 }
+
 
